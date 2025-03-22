85d62915e8ea7eb65689887884aec3b193e32518
Renan Cecolin <rececolin@gmail.com>
2025-03-22 17:23:32 -0300

    Corrigindo alguns erros de codificação

--- frontend/app/src/main/java/br/edu/puccampinas/frontend/Avaliar.kt
+++ frontend/app/src/main/java/br/edu/puccampinas/frontend/Avaliar.kt
@@ -11,10 +11,5 @@ class Avaliar : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_avaliar)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
-}
--- frontend/app/src/main/java/br/edu/puccampinas/frontend/Oportunidades.kt
+++ frontend/app/src/main/java/br/edu/puccampinas/frontend/Oportunidades.kt
@@ -20,22 +20,5 @@ class Oportunidades : AppCompatActivity() {
         binding = ActivityOportunidadesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_activity_oportunidades)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_activity_oportunidades)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
